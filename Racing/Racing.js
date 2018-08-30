73ff6ef75020a05ab25dc0fc83a10fc7d064553e
caishu1995 <2687366516@qq.com>
2018-08-30 15:29:56 +0800

    1.1.5. 完成游戏        新增车辆

--- Racing/Racing.js
+++ Racing/Racing.js
@@ -50,6 +50,12 @@ var Racing = {
                 wayColor: "606A7C",
                 middleColor: 'FFF',
                 length: 5000        //车道长度
+            },
+            car: {
+                width : 160,
+                left: (width / 2) - 80,
+                top: 320,
+                height : 100
             }
         };
     },
@@ -66,7 +72,6 @@ var Racing = {
 
     init: function() {
         this.context = document.getElementById(this.baseCanvas).getContext("2d");
-
 
         ///初始化配置文件
         this.config(document.body.scrollHeight, document.body.scrollWidth);
@@ -93,7 +98,6 @@ var Racing = {
     },
     //定时器事件
     timeInterval: function(speedRecord) {
-
         //如果车速改变，则改变车速
         if(this.parameter.speed != speedRecord.lastSpeed) {
             speedRecord.lastSpeed = this.parameter.speed;
@@ -105,7 +109,7 @@ var Racing = {
         var context = this.context;
         context.clearRect(0, 0, this.config.size.width, this.config.size.height);    //清空区域
 
-        this.drawSky(context, this.config.size.width, this.config.skyHeight);//画天空
+        this.roundedRectangle(context, "#D4F5FE", 0, 0, this.config.size.width, this.config.skyHeight, 0);//画天空
         //画山峰们
         for(var i in this.config.mountain){
             this.drawMountain(context, this.config.mountain[i].siteX, this.config.mountain[i].siteY, this.config.mountain[i].width, this.config.mountain[i].height, this.parameter.fieldDeviation);
@@ -113,6 +117,7 @@ var Racing = {
         this.drawLand(context, this.config.size.width, this.config.size.height - this.config.skyHeight, this.config.skyHeight, this.config.landColorList, this.parameter.landFirstColorIndex);//画地
         this.drawRoad(context, this.config.size.width, this.config.size.height - this.config.skyHeight, this.config.skyHeight, this.parameter.fieldDeviation, this.config.road);//画街道
         this.drawIndexDial(context, this.config.size.width - 60, this.config.size.height - 60, 135 + this.parameter.speed * 45);//画车速
+        this.drawCar(context, this.config.car.left, this.config.car.top, this.config.car.width, this.config.car.height);       //画车
 
         this.context = context;
 
@@ -130,24 +135,13 @@ var Racing = {
     },
 
 
-    ///画天空
-    /// height：高度
-    /// width ： 宽度
-    drawSky: function(context, width, height) {
-        context.save();
-
-        context.fillStyle = "#D4F5FE";
-        context.strokeStyle = "#D4F5FE";
-        context.fillRect(0, 0, width, height);
-        context.fill();
-
-        context.restore();
-    },
     ///画地
-    /// height：高度
-    /// width ： 宽度
-    /// beginHeight ： 开始高度
-    /// landConfig ： 地配置数据
+    /// context：画布
+    /// height ：高度
+    /// width  ：宽度
+    /// beginHeight：开始高度
+    /// landColor  ：地颜色列表
+    /// index  ：开始的序号
     drawLand: function(context, width, height, beginHeight, landColor, index){
         var hasDrawHeight = 0; //已画高度
         var i = 3;            //此次长方形的高
@@ -166,11 +160,12 @@ var Racing = {
         context.restore();
     },
     ///画道路
-    /// height：高度
-    /// width ： 宽度
-    /// beginHeight ： 开始高度
-    /// deviation   ： 偏移量
-    /// roadConfig  ： 道路配置数据
+    /// context：画布
+    /// height ：高度
+    /// width  ：宽度
+    /// beginHeight：开始高度
+    /// deviation  ：偏移量
+    /// roadConfig ：道路配置数据
     drawRoad: function(context, width, height, beginHeight, deviation, roadConfig){
         context.save();
 
@@ -219,6 +214,7 @@ var Racing = {
     /// top   ：开始点的上边距
     /// height：高度
     /// width ： 宽度
+    /// fieldDeviation ： 偏移量
     drawMountain: function(context, left, top, width, height, fieldDeviation) {
         context.save();
 
@@ -251,7 +247,7 @@ var Racing = {
 
         //画盘
         context.beginPath();
-        context.arc(0, 0, 50, Math.PI * 3 / 4, Math.PI * 1 / 4, false);//创建圆
+        context.arc(0, 0, 45, Math.PI * 3 / 4, Math.PI * 1 / 4, false);//创建圆
         context.stroke();     //绘制轮廓形状
 
         //画刻度
@@ -259,19 +255,14 @@ var Racing = {
         for(var i = 0; i < 7; i++){
             context.rotate(Math.PI * 1 / 4); //旋转
             context.beginPath();
-            context.moveTo(55,0);
-            context.lineTo(37, 0);//创建线
+            context.moveTo(50,0);
+            context.lineTo(30, 0);//创建线
             context.stroke();     //绘制轮廓形状
         }
 
-        //画中心点
-        context.beginPath();
-        context.arc(0, 0, 3, 0, Math.PI * 2, false);//创建圆
-        context.stroke();     //绘制轮廓形状
-        context.fillStyle = "white"; //设置填充颜色，默认为黑色
-        context.fill();              //填充
-
         context.restore();//恢复上次保存的状态
+
+        this.light(context, "white", pointX, pointY, 5);
 
 
         ///画指针
@@ -282,13 +273,82 @@ var Racing = {
         context.lineWidth = 5;         //框宽
         context.rotate(Math.PI * angle / 180); //旋转
         context.beginPath();
-        context.moveTo(7,0);
-        context.lineTo(30, 0);//创建线
+        context.moveTo(5,0);
+        context.lineTo(25, 0);//创建线
         context.stroke();     //绘制轮廓形状
 
         context.restore();//恢复上次保存的状态
-    }
+    },
+    ///画车
+    /// context：画布
+    /// left   ：区域左侧
+    /// top    ：区域顶部
+    /// width  ：区域宽度
+    /// height ：区域高度
+    drawCar: function(context, left, top, width, height) {
+        this.roundedRectangle(context, "#FFF", left + 90, top + height - 100, 10, 10, 0);             // 车头
+        this.roundedRectangle(context, "#FFF", left + 30, top + height - 90, width - 60, 40, 7, 10);  // 车后档
+        this.roundedRectangle(context, "#C2C2C2", left + 7, top + height - 75, width - 14, 34, 17);   // 车顶
+        this.roundedRectangle(context, "#DEE0E2", left + 27, top + height - 75, width - 54, 40, 0, 10);  // 车后阴影
+        this.roundedRectangle(context, "rgba(0, 0, 0, 0.35)", left, top + height - 5, width, 20, 9); // 车下阴影
+        this.roundedRectangle(context, "#111", left, top + height - 5, 30, 13, 6);                   // 左车轮
+        this.roundedRectangle(context, "#111", left + width - 30, top + height - 5, 30, 13, 6);      // 右车轮
+        this.roundedRectangle(context, "#DEE0E2", left - 6, top + height - 50, width + 12, 50, 12);  // 车底座
+        this.light(context, "#FF9166", left + 10, top + height - 35, 5);         // 车灯1
+        this.light(context, "#FF9166", left + 25, top + height - 35, 5);         // 车灯2
+        this.light(context, "#FF9166", left + width - 10, top + height - 35, 5); // 车灯3
+        this.light(context, "#FF9166", left + width - 25, top + height - 35, 5); // 车灯4
+        this.roundedRectangle(context, "#FFF", left + width / 2 - 20, top + height - 33, 40, 17, 5); // 车牌
+        this.roundedRectangle(context, "#474747", left - 2, top + height - 55, width + 4, 10, 3);    // 后杠1
+        this.roundedRectangle(context, "#474747", left + 50, top + height - 50, width - 100, 10, 5); // 后杠2
+    },
 
+
+    ///画圆角四边形
+    /// context：画布
+    /// color  ：颜色
+    /// x：左边距
+    /// y：上边距
+    /// width ： 宽度
+    /// height ： 高度
+    /// radius ： 圆角半径
+    /// inclination ： 斜度
+    roundedRectangle: function (context, color, x, y, width, height, radius, inclination) {
+        if(inclination == undefined) inclination = 0;
+
+        context.save();
+
+        context.fillStyle = color;
+        context.beginPath();
+        context.moveTo(x + radius + inclination, y);
+        context.lineTo(x + width - inclination - radius, y);
+        context.arcTo(x + width - inclination, y, x + width - inclination, y + 10, radius);
+        context.lineTo(x + width, y + height - radius);
+        context.arcTo(x + width, y + height, x + width - radius, y + height, radius);
+        context.lineTo(x + radius, y + height);
+        context.arcTo(x, y + height, x, y + height - radius, radius);
+        context.lineTo(x + inclination, y + radius);
+        context.arcTo(x + inclination, y, x + radius + inclination, y, radius);
+        context.fill();
+
+        context.restore();
+    },
+    ///画实心圆
+    /// context：画布
+    /// color  ：颜色
+    /// x： 左边距
+    /// y： 上边距
+    /// r： 半径
+    light: function(context, color, x, y, r) {
+        context.save();
+
+        context.beginPath();
+        context.arc(x, y, r, 0, Math.PI * 2, false);
+        context.fillStyle = color;
+        context.fill();
+
+        context.restore();
+    }
 };
 Racing.init();
 
@@ -304,10 +364,10 @@ document.onkeydown = function(e) {
     }
     if(e.keyCode == 37) { //left
         Racing.parameter.fieldDeviation += 10;
-        if(Racing.parameter.fieldDeviation >= 250) Racing.parameter.fieldDeviation = 250;
+        if(Racing.parameter.fieldDeviation >= 165) Racing.parameter.fieldDeviation = 165;
     }
     if(e.keyCode == 39) { //right
         Racing.parameter.fieldDeviation -= 10;
-        if(Racing.parameter.fieldDeviation <= -250) Racing.parameter.fieldDeviation = -250;
+        if(Racing.parameter.fieldDeviation <= -165) Racing.parameter.fieldDeviation = -165;
     }
 };
